a7ddd4a89ef534badc6da95c1df1c60fd392f50c
frisko-sposad <safiko@ya.ru>
2020-10-07 09:31:25 +0300

    feat: add movie choice

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -4,12 +4,22 @@ import MovieInfo from './MovieInfo/MovieInfo';
 import style from './Header.module.css';
 
 const Header = () => {
-  return (
-    <header className={style.Header}>
-      <div><SearchSection /></div>
-      {/* <div><MovieInfo /></div> */}
-    </header>
-  );
+  let search = true;
+  if(search) {
+    return (
+      <header className={style.Header}>
+        <div><SearchSection /></div>        
+      </header>
+    );
+  } else {
+    return (
+      <header className={style.Header}>        
+        <div><MovieInfo /></div>
+      </header>
+    );
+  }
+  
+  
 };
 
 export default Header;
