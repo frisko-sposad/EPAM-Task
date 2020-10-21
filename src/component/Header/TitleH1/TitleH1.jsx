e9258eece1829375674f65e967c56d11f5cafcf8
frisko-sposad <safiko@ya.ru>
2020-10-21 11:56:39 +0300

    delete TitleH1

--- src/component/Header/TitleH1/TitleH1.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import style from './TitleH1.module.css';
-
-const TitleH1 = () => {
-  return <h1 className={style.h1}>NetflixRoulette</h1>;
-};
-
-export default TitleH1;
